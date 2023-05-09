10a84a8fbc525f2d4078118b38172dcc0fdc5e28
Resty <54195157+R3sty@users.noreply.github.com>
2023-05-09 23:51:02 +0900

    added navbar, logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/navbar/Navbar"
+
 
 export default function Home() {
   return (
     <main>
+      <Navbar/>
       <div className="text-teal-600">Page works!</div>
     </main>
   )
